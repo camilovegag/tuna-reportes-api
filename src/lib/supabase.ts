36d646011b816c74b632855f67fd05ff8f7531b0
camilovegag <camilovegag@unisabana.edu.co>
2025-05-22 19:51:14 -0500

    Configure supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase URL or Anon Key in environment variables");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
