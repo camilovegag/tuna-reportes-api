6686782ff87f93994fc78e20790417b5c928a7e6
camilovegag <camilovegag@unisabana.edu.co>
2025-07-04 19:36:55 -0500

    Update password login schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -10,9 +10,10 @@ export const registerSchema = z.object({
   password: z.string().min(8, "Password must be at least 8 characters"),
   vinculationCode: z.uuid(),
 });
+
 export const loginSchema = z.object({
   email: z.email(),
-  password: z.string(),
+  password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
 export type RegisterInput = z.infer<typeof registerSchema>;
