ba8c8c03dd950acc93168db3f2a42ad3ad628377
camilovegag <camilovegag@unisabana.edu.co>
2025-07-07 22:46:40 -0500

    Create member types for get and post responses

--- src/types/member.ts
+++ src/types/member.ts
@@ -3,3 +3,13 @@ import { dbSchema } from "../db";
 export type Member = typeof dbSchema.members.$inferSelect;
 
 export type MemberInsert = typeof dbSchema.members.$inferInsert;
+
+export type MembersGetResponse = {
+  members: Member[];
+  count: number;
+};
+
+export type MemberPostResponse = {
+  id: string;
+  message: string;
+};
